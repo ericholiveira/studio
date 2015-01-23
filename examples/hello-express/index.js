01b450da5696b7f2e05f13017e322e80e027b471
Erich Oliveira <ericholiveira10@gmail.com>
2015-01-23 17:37:12 -0200

    adding examples

---
+++ examples/hello-express/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var Studio = require('../../compiled/core/studio');
+var app = express();
+var driver = new Studio.Driver({
+	initialize: function () {
+		app.get('/', function (req, res) {
+			driver.send(req, res).then(function (message) {
+				res.send(message);
+			});
+		});
+	},
+	parser: function (req, res) {
+		return {
+			sender: 'expressDriver',
+			receiver: 'helloActor',
+			body: null,
+			headers: null
+		};
+	}
+});
+var hello = new Studio.Actor({
+	id: 'helloActor',
+	process: function (body, headers, sender, receiver) {
+		return 'Hello World!!!';
+	}
+});
+app.listen(3000);
+console.log('Hello World application running on http://localhost:3000');
