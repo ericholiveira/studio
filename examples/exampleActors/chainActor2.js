e01766ceec3d20e6b1299fd1e7c690afdd91eeff
ericholiveira <ericholiveira10@gmail.com>
2015-08-27 19:45:11 -0300

    example of actor factory

--- examples/exampleActors/chainActor2.js
+++ examples/exampleActors/chainActor2.js
@@ -1,10 +1,24 @@
 var Studio = require('../../compiled/core/studio');
 
+/*
+This is another way to instatiate an actor, this is the same as:
+
+Studio.actorFactory({chainActor2:function (body, headers, sender, receiver) {
+  console.log('Received message to actor = ' + this.id);
+  return body + 'actor2';
+}});
+
+OR
 
 var chainActor2 = new Studio.Actor({
   id: 'chainActor2',
-  process: function(body, headers, sender, receiver) {
-    console.log('Received message to actor = ' + chainActor2.id);
+  process:function(body, headers, sender, receiver) {
+    console.log('Received message to actor = ' + this.id);
     return body + 'actor2';
   }
+});
+*/
+Studio.actorFactory(function chainActor2(body, headers, sender, receiver) {
+  console.log('Received message to actor = ' + this.id);
+  return body + 'actor2';
 });
