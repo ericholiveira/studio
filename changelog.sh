6ba245360de5bd1075331ebd83943a5a7136ed8f
Erich Oliveira <ericholiveira10@gmail.com>
2015-10-09 17:19:54 -0300

    update change log

--- changelog.sh
+++ changelog.sh
@@ -1,2 +1,10 @@
-formatLog=$'* [commit](http://github.com/ericholiveira/studio/commit/%H) %s \n %b'
-gitLogsResult=`git log --pretty=format:"$formatLog" --all  --no-merges | grep -v "release 0." >> CHANGELOG.md`
+#!/bin/bash
+tagDate=`date +'%d %b %Y %H:%M:%S'`
+echo "### $1 - $tagDate" > LASTCHANGES_CHANGELOG.md
+formatLog=$'* [commit](http://github.com/ericholiveira/studio/commit/%H) %s\n %b'
+gitLogsResult=`git log "$2"... --pretty=format:"$formatLog" --no-merges | grep -v "release 0." >> LASTCHANGES_CHANGELOG.md`
+mv CHANGELOG.md _TEMPCHANGELOG.md
+cat *CHANGELOG.md > CHANGELOG.md
+rm -f _TEMPCHANGELOG.md
+git commit CHANGELOG.md  -m "release $1 CHANGELOG.md"
+git commit LASTCHANGES_CHANGELOG.md  -m "Setting version to $nextTag LASTCHANGES_CHANGELOG.md"
