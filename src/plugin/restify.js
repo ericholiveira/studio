416bb46989b96a73439b0bb948943466c894bc38
Erich Oliveira <ericholiveira10@gmail.com>
2016-02-05 15:21:17 -0200

    finish auto documentation

--- src/plugin/restify.js
+++ src/plugin/restify.js
@@ -1,22 +1,55 @@
 var swaggerInfo ={};
 var paths = {};
-var paramsDoc = [];
+var definitions = {};
 var startSwagger = function(swagger,server){
     swaggerInfo = swagger;
     swaggerInfo.swagger = '2.0';
     swaggerInfo.paths = paths;
+    swaggerInfo.definitions = definitions;
     server.get('/documentation',function(req,res,next){
         res.send(swaggerInfo);
         next();
     });
 };
-var addPathToSwagger = function(opt){
+var addPathToSwagger = function(opt,paramsDoc){
     var helper = {};
+    opt.path = "\""+opt.path.replace(/:([\w]*)/g,'{$1}')+"\"";
     helper[opt.path] = {};
     helper[opt.path][opt.method] = opt.meta;
     paths[opt.path] = paths[opt.path] || helper[opt.path];
     paths[opt.path][opt.method] = helper[opt.path][opt.method];
     paths[opt.path][opt.method].parameters = paramsDoc;
+};
+var createModel = function(modelName,fields){
+    var i,properties={};
+    if(definitions[modelName]){
+        return;
+    }
+    definitions[modelName]={type:'object',properties:properties};
+    for(i=0;i<fields.length;i++){
+        definitions[modelName].properties[fields[i].key] = {
+            type: fields[i].schema._type
+        }
+    }
+};
+var mapDocs = function(v){
+    var type;
+    var result = {
+        name: v.parameter,
+        in: v.location,
+        required: v.validator._flags.presence === 'required'
+    };
+    if(v.validator && v.validator.isJoi){
+        type = v.validator._meta && v.validator._meta[0] && v.validator._meta[0].type || v.validator._type;
+        if(v.validator._type === 'object' && v.validator._type !== type ){
+            createModel(type, v.validator._inner.children);
+            result.schema = {$ref:'#/definitions/'+type}
+        }else{
+            result.type = type;
+        }
+        result.description = v.validator._description || '';
+    }
+    return result;
 };
 var pluginFn = function(server,opt){
     var _errorHandler = opt.errorHandler || function(err,req,res,next){
@@ -25,28 +58,38 @@ var pluginFn = function(server,opt){
         return next(true);
     };
     startSwagger(opt.swagger,server);
-    return function(options){
+    return function(options,Studio){
         options.onStart(function(service,ref){
             ref.restify = function(opt){
+                var paramsDoc;
                 opt.errorHandler = opt.errorHandler || _errorHandler;
                 opt.method = (opt.method || 'GET').toLowerCase();
                 opt.params = opt.params || [];
-                opt.paramsDoc = paramsDoc;
-                addPathToSwagger(opt);
-                paramsDoc = [];
+                var validators = opt.params.map(function(v){
+                    return v.validator;
+                });
+                paramsDoc = opt.params.map(mapDocs);
+                addPathToSwagger(opt,paramsDoc);
+                _fn = service.fn;
+                service.fn=Studio.promise.method(function(){
+                    var args = [].slice.call(arguments);
+                    var i = 0,_len = Math.min(args.length,validators.length);
+                    var value;
+                    for(;i<_len;i++){
+                        if(validators[i]){
+                            value = validators[i].validate(args[i]);
+                            if(value.error){
+                                throw value.error;
+                            }
+                        }
+                    }
+                    return _fn.apply(service,args);
+                });
                 server[opt.method](opt.path,function(req,res,next){
                     var i = 0,_len = opt.params.length;
                     var params = [];
-                    var value;
                     for(;i<_len;i++){
-                        value = opt.params[i](req);
-                        if(value.validator) {
-                            value = value.validator.validate(value.param);
-                        }
-                        if(value.error){
-                            return opt.errorHandler(value.error,req,res,next);
-                        }
-                        params.push(value.value);
+                        params.push(opt.params[i](req));
                     }
                     ref.apply(ref,params).then(function(result){
                         res.send(result);
@@ -60,40 +103,47 @@ var pluginFn = function(server,opt){
         });
     };
 };
-var addParamDoc = function(location,parameter,validator){
-    if(validator && validator.isJoi){
-        paramsDoc.push({
-            "name": parameter,
-            "in": location,
-            "required": validator._flags.presence === 'required',
-            "type": validator._type
-        });
-    }
+pluginFn.reqParam = function(parameter){
+    var getter = function(req){
+        return req[parameter];
+    };
+    return getter;
 };
 pluginFn.pathParam = function(parameter,validator){
-    addParamDoc('path',parameter,validator);
-    console.log(validator);
-    return function(req){
-        return {param:req.params[parameter],validator:validator};
+    var getter = function(req){
+        return req.params[parameter];
     };
+    getter.location = 'path';
+    getter.parameter = parameter;
+    getter.validator = validator;
+    return getter;
 };
 pluginFn.bodyParam = function(parameter,validator){
-    addParamDoc('body',parameter,validator);
-    return function(req){
-        return {param:req.body[parameter],validator:validator};
+    var getter = function(req){
+        return req.body;
     };
+    getter.location = 'body';
+    getter.parameter = parameter;
+    getter.validator = validator;
+    return getter;
 };
 pluginFn.queryParam = function(parameter,validator){
-    addParamDoc('query',parameter,validator);
-    return function(req){
-        return {param:req.query[parameter],validator:validator};
+    var getter = function(req){
+        return req.query[parameter];
     };
+    getter.location = 'query';
+    getter.parameter = parameter;
+    getter.validator = validator;
+    return getter;
 };
 pluginFn.headerParam = function(parameter,validator){
-    addParamDoc('header',parameter,validator);
-    return function(req){
-        return {param:req.headers[parameter],validator:validator};
+    var getter = function(req){
+        return req.headers[parameter];
     };
+    getter.location = 'header';
+    getter.parameter = parameter;
+    getter.validator = validator;
+    return getter;
 };
 
 module.exports = pluginFn;
