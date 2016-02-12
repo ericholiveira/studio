e2cdaa33087b22aadf1a679c72c56d1dd6a6abc0
Erich Oliveira <ericholiveira10@gmail.com>
2016-02-12 15:59:08 -0200

    performance improvement

--- src/plugin/timer.js
+++ src/plugin/timer.js
@@ -1,5 +1,5 @@
 var calculateResult = function(start,receiver,err){
-    var end = new Date().getTime();
+    var end = Date.now();
     return {
         time:end - start,
         receiver:receiver,
@@ -11,7 +11,7 @@ module.exports = function(fn) {
         options.onStart(function (serv) {
             var _fn = serv.fn;
             serv.fn = function(){
-              var start = new Date().getTime();
+              var start = Date.now();
               return _fn.apply(serv,arguments).then(function (res) {
                 fn(calculateResult(start,serv.id));
                 return res;
--- src/router.js
+++ src/router.js
@@ -16,13 +16,20 @@ Router.prototype.deleteRoute = function(id){
 
 Router.prototype.send = function(receiver){
     return function(){
-        var params = [].slice.call(arguments);
-        var message = clone(params);
-        var route = _routes[receiver];
-        if(route){
-            return route.fn.apply(route,message);
+      var route = _routes[receiver];
+      if(!route){
+        return _Promise.reject(exceptions.RouteNotFoundException(arguments[arguments.length -1]));
+      }
+      switch(arguments.length){
+          case 0: 
+            return route.fn();
+          case 1:
+            return route.fn(clone(arguments[0]));
+          case 2:
+            return route.fn(clone(arguments[0]),clone(arguments[1]));
+          default: 
+            return route.fn.apply(route,clone([].slice.call(arguments)));
         }
-        return _Promise.reject(exceptions.RouteNotFoundException(params[params.length-1]));
     };
 };
 
