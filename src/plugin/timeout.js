5dfe8ecb3bf8257b7886586dbe58a5585105fb04
Erich Oliveira <ericholiveira10@gmail.com>
2016-02-22 17:14:11 -0300

    fix timeout plugin

--- src/plugin/timeout.js
+++ src/plugin/timeout.js
@@ -1,9 +1,9 @@
 module.exports = function(options){
   "use strict";
     options.onStart(function(serv,ref){
-        var _fn = serv.fn;
         ref.timeout = function(ts){
-            serv.fn = function(){
+          var _fn = serv.fn;
+          serv.fn = function(){
                 var body = [].slice.call(arguments);
                 return _fn.apply(serv,body).timeout(ts);
             };
