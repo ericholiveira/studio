fac19f34a4f0931d5216eb4f3054c24ff1cee3a6
Erich Oliveira <ericholiveira10@gmail.com>
2016-08-19 15:34:42 -0300

    Studio main methods documentation

--- src/studio.js
+++ src/studio.js
@@ -6,7 +6,26 @@ var isGeneratorFunction = require('./util/generator').isGeneratorFunction;
     This is the main object exposing all Studio methods
 */
 var _Studio= {
+    /** Studio internal router, avoid usage*/
     router: require('./router'),
+    /** @function
+    * @name service
+    * @desc Creates a service from a named function . Or from a object with parameters id and fn
+    * @param {function} fn. The service
+    * @example
+    * Studio.service(function myService(){
+    *    // DO SOMETHING
+    * });//Creates a service called myService
+    * Studio.service({
+    *   id:'myOtherService',
+    *   fn:function (){
+    *       // DO SOMETHING
+    *   }
+    * });//Creates a service called myOtherService
+    * Studio(function yetAnotherService(){
+    *    // DO SOMETHING
+    * });// Studio(SOME_FUNCTION) and Studio(SOME_OBJECT) is a shortcut for Studio.service
+    */
     service: require('./service'),
     /** @function
     * @name serviceClass
@@ -69,6 +88,30 @@ var _Studio= {
     * });
     */ 
     defer:require('./util/promise_handlers'),
+    /** @function
+    * @name module
+    * @desc Creates (or retrieves if it already exists) a module. 
+    * Should be used to avoid service name conflict for large systems.
+    * The module created have all the same methods and properties of Studio object, but running inside of that namespace.
+    * @param {String} moduleName. The module name
+    * @return {function} Studio. A function with all methods and properties fo Studio
+    * @example
+    * var fooModule = Studio.module('foo'); // Create or retrieves a module
+    * var barModule = Studio.module('bar'); // Create or retrieves a module
+    * // Create a service inside foo module, this service calls a service from bar module
+    * fooModule(function fooMethod(){
+    *   var barService = barModule('barMethod');
+    *   return barservice('Hello');
+    * });
+    * // Creates a service inside bar module
+    * barModule(function barMethod(param){
+    *   return param = ' bar';
+    * });
+    * var fooService = fooModule('fooMethod');//Retrieves fooMethod from foo module
+    * fooService().then(function(result){
+    *   console.log(result); // Prints Hello bar'
+    * });
+    */
     module:function(moduleName){
       "use strict";
         var result = function(options,extra){
@@ -93,6 +136,22 @@ var _Studio= {
         };
         return result;
     },
+    /** @function
+    * @name use
+    * @desc Enables a plugin
+    * @param {function} plugin. The plugin you want to use
+    * @param {Object} filter. The filter for this plugin usage
+    * @example
+    * Studio.use(myPlugin);//Enables myPlugin for all services
+    * Studio.use(otherPlugin,'foo'); // Enables otherPlugin only for a service called foo
+    * Studio.use(otherPlugin2,/f.+/); // Enables otherPlugin2 only for services which names match the /f.+/ regex
+    * Studio.use(otherPlugin3,function(serviceName){
+    *   return serviceName.length===3;
+    * }); // Enables otherPlugin3 only for services which names satisfies that function
+    * Studio.use(otherPlugin4,['foo',/f.+/,function(serviceName){
+    *   return serviceName.length===3;
+    * }]);// Enables otherPlugin4 only for services which names satisfies ANY filter in the array
+    */
     use : function(plugin,filter){
         var self = this;
         return plugin({
@@ -101,7 +160,16 @@ var _Studio= {
             interceptSend:function(listener){require('./util/listeners').addInterceptSend(listener,filter,self._moduleName);}
         },Studio);
     },
+    /** @function
+    * @name ref
+    * @desc Gets a reference to a service by his name
+    * @param {String} name. The service name
+    * @example
+    * var myServiceRef = Studio.ref('myService');//Returns a reference for a service called myService
+    * var otherMyServiceRef = Studio('myService');// Studio(SOME_STRING) is a shortcut for Studio.ref(SOME_STRING)
+    */
     ref : require('./ref'),
+    /** The core plugins available*/
     plugin : {
         timer:require('./plugin/timer'),
         timeout:require('./plugin/timeout'),
@@ -130,6 +198,19 @@ if(typeof Proxy !=='undefined'){
             }
         });
     };
+    /** @function
+    * @name services
+    * @desc Gets a reference for all services available on Studio. 
+    * This function is available only if Proxy is enabled (node > 6 or older version with --harmony-proxies flag)
+    * @example
+    * var allServices = Studio.services();// A reference for all services
+    * var myServiceRef = Studio('myService');//Returns a reference for a service called myService
+    * var myServiceRefFromProxy = allServices.myService;//a reference for a service called myService
+    * // For an object inside a module
+    * var fooModule = Studio.module('foo');
+    * var barServiceRef = fooModule('bar');
+    * var barServiceRefFromProxy = allServices.foo.bar;
+    */
     _Studio.services=function(){
         return createProxy(this);
     };
