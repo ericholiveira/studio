16fc0f5dacde176401c2cae2a50e9d1ea3ae1783
Erich Oliveira <ericholiveira10@gmail.com>
2016-10-25 16:18:41 -0200

    exponential backoff implementation

--- src/plugin/retry.js
+++ src/plugin/retry.js
@@ -1,24 +1,36 @@
-module.exports = function(options){
+module.exports = function(defaultOptions){
     "use strict";
-    options.onStart(function(serv,ref){
-        ref.retry = function(retryCount,filter){
-            var _fn = serv.fn;
-            retryCount = +retryCount || 0;
-            filter = filter || function(){return true;};
-            serv.fn = function(){
-                var body = arguments;
-                var call = function(retries){
-                    return _fn.apply(serv,body).catch(function(error){
-                        if(retries > 0 && filter(error)){
-                            return call(retries -1);
-                        }else{
-                            throw e;
-                        }
-                    });
+    defaultOptions = defaultOptions || {};
+    return function(options,Studio){
+        options.onStart(function(serv,ref){
+            ref.retry = function(localOptions){
+                var _fn = serv.fn;
+                localOptions = localOptions || {};
+                var filter = localOptions.filter || defaultOptions.filter || function(){return true;};
+                var retryCount = +(localOptions.max || defaultOptions.max || 0);
+                var initialInterval = +(localOptions.initialInterval || defaultOptions.initialInterval || 0);
+                var factor = +(localOptions.factor || defaultOptions.factor || 1);
+                filter = filter || function(){return true;};
+                serv.fn = function(){
+                    var body = arguments;
+                    var call = function(retries,interval){
+                        return _fn.apply(serv,body).catch(function(error){
+                            if(retries > 0 && filter(error)){
+                                return new Studio.promise.delay(interval).then(function(){
+                                    return call(retries -1, interval*factor);
+                                });
+                            }else{
+                                throw e;
+                            }
+                        });
+                    };
+                    return call(retryCount,initialInterval);
                 };
-                return call(retryCount);
+                return ref;
             };
-            return ref;
-        };
-    });
+        });
+    };
 };
+
+
+
--- tests/plugin_retry_test.js
+++ tests/plugin_retry_test.js
@@ -2,7 +2,7 @@ var expect = require("chai").expect;
 var Studio = require('../src/studio');
 Studio = Studio.module('plugin_retry');
 describe("Retry plugin",function(){
-    Studio.use(Studio.plugin.retry);
+    Studio.use(Studio.plugin.retry());
     Studio.use(Studio.plugin.timeout);
     var noRetry_RetryCount = 0;
     Studio(function noRetry(){
@@ -21,7 +21,7 @@ describe("Retry plugin",function(){
     Studio(function retry1NoFilter(){
         retry1NoFilter_RetryCount++;
         throw new Error('noRetry');
-    }).retry(1);
+    }).retry({max:1});
     var retry1NoFilterService = Studio('retry1NoFilter');
     it("must retry without filters one time",function(done){
         retry1NoFilterService().then(done).catch(function(e){
@@ -35,7 +35,7 @@ describe("Retry plugin",function(){
         retry5NoFilter_RetryCount++;
         throw new Error('noRetry');
 
-    }).retry(5);
+    }).retry({max:5});
     var retry5NoFilterService = Studio('retry5NoFilter');
     it("must retry without filters several times",function(done){
         retry5NoFilterService().then(done).catch(function(e){
@@ -48,13 +48,49 @@ describe("Retry plugin",function(){
     Studio(function retryFilter(){
         retryFilter_RetryCount++;
         throw new Error(retryFilter_RetryCount);
-    }).retry(10,function(e){
+    }).retry({max:10,filter:function(e){
         return +e.message < 3;
-    });
+    }});
     var retryFilterService = Studio('retryFilter');
     it("must retry with filters several times",function(done){
         retryFilterService().then(done).catch(function(e){
             expect(retryFilter_RetryCount).to.equal(3);
+            done();
+        });
+    });
+
+
+
+
+    var retry1NoFilterWithInterval_RetryCount = 0;
+    Studio(function retry1NoFilterWithInterval(){
+        retry1NoFilterWithInterval_RetryCount++;
+        throw new Error('noRetry');
+    }).retry({max:1,initialInterval:10});
+    var retry1NoFilterWithIntervalService = Studio('retry1NoFilterWithInterval');
+    it("must retry with initial interval one time",function(done){
+        var initialTime = new Date().getTime();
+        retry1NoFilterWithIntervalService().then(done).catch(function(e){
+            var now = new Date().getTime();
+            expect(retry1NoFilterWithInterval_RetryCount).to.equal(2);
+            expect(now - initialTime).to.gte(10);
+            done();
+        });
+    });
+
+
+    var retry2NoFilterWithIntervalWithExponentialFactor_RetryCount = 0;
+    Studio(function retry2NoFilterWithIntervalAndExponentialFactor(){
+        retry2NoFilterWithIntervalWithExponentialFactor_RetryCount++;
+        throw new Error('noRetry');
+    }).retry({max:2,initialInterval:5,factor:3});
+    var retry2NoFilterWithIntervalAndExponentialFactorService = Studio('retry2NoFilterWithIntervalAndExponentialFactor');
+    it("must retry with initial interval one time",function(done){
+        var initialTime = new Date().getTime();
+        retry2NoFilterWithIntervalAndExponentialFactorService().then(done).catch(function(e){
+            var now = new Date().getTime();
+            expect(retry2NoFilterWithIntervalWithExponentialFactor_RetryCount).to.equal(3);
+            expect(now - initialTime).to.gte(5+5*3);
             done();
         });
     });
