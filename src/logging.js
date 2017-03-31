651f23b89638ba32f2d42700962dd3794ffe1bfd
Erich Oliveira <ericholiveira10@gmail.com>
2017-03-31 09:39:36 -0300

    fix lint

--- src/logging.js
+++ src/logging.js
@@ -9,11 +9,11 @@ function Logger(config) {
 
 Logger.prototype.log = function Logger_log(data) {
     this.debugger(data.replace('%{nameSpace}', this.nameSpace));
-}
+};
 
 Logger.prototype.error = function Logger_log(data) {
     this.debugger('ERROR: '+data.replace('%{nameSpace}', this.nameSpace));
-}
+};
 
 module.exports = {
     instance: new Logger(),
